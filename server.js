12e085b7df56593e2e04c8c64fe9908d64c85a6a
erezlevanon <erez.levanon@mail.huji.ac.il>
2021-02-07 12:36:22 +0200

    second try base app

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Create link to Angular build directory
+const distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 const server = app.listen(process.env.PORT || 8080, function () {
   const port = server.address().port;
   console.log("App now running on port", port);
