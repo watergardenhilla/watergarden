ce3b4250cd9e5f3501a6fddda852165353a38bce
erezlevanon <erez.levanon@mail.huji.ac.il>
2021-02-08 20:23:47 +0200

    working for desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AsyncSubject, BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
   title = 'watergarden';
-  rows = [0, 1, 2, 3, 4, 5, 6];
-  columns = [0, 1, 2, 3, 4, 5, 6];
+
+  showTitle = new BehaviorSubject(true);
+  @ViewChild('displaypart') container: ElementRef;
+  @ViewChild('header') header: ElementRef;
+  rows = [];
+  columns = [];
+  readonly dropWidth = 48;
+  readonly dropRightMargin = 4.55;
+  readonly dropBottomMargin = 9;
+  displayMarginLeft = new AsyncSubject();
+
+  constructor(private cdRef: ChangeDetectorRef) {
+  }
+
+  hideTitle(): void {
+    this.showTitle.next(false);
+  }
+
+  ngAfterViewInit(): void {
+    for (let i = this.dropWidth;
+         i < this.container.nativeElement.offsetWidth;
+         i += this.dropWidth + this.dropRightMargin) {
+      this.rows.push(0);
+      console.log(this.container.nativeElement.offsetWidth - i);
+      this.displayMarginLeft.next((this.container.nativeElement.offsetWidth - i + this.dropWidth + this.dropRightMargin) / 3);
+    }
+    for (let i = this.dropWidth;
+         i < this.container.nativeElement.offsetHeight;
+         i += this.dropWidth + this.dropBottomMargin) {
+      this.columns.push(0);
+    }
+    this.displayMarginLeft.complete();
+  }
+
+  ngAfterViewChecked(): void {
+    this.cdRef.detectChanges();
+  }
 }
